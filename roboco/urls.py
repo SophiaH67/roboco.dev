ee6cd625a48b6e6b32e358f4da27a4875a685fb1
mc-server <marnix@hagehoeve.com>
2022-06-05 14:58:58 +0200

    feat: profile url

--- roboco/urls.py
+++ roboco/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from roboco import views
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,8 +25,17 @@ urlpatterns = [
     path("why", views.why, name="why"),
     # User views
     path("register/", user_views.register, name="register"),
-    # path("profile/", user_views.profile, name="profile"),
-    # path("login/", user_views.login, name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="users/logout.html"),
+        name="logout",
+    ),
+    path("profile/", user_views.profile, name="profile"),
     # Django Reload
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import redirect, render
 from users.forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+
 
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("index")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
 
 
-# def login(request):
-#     if request.method == "POST":
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
