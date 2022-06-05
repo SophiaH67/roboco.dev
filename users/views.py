f934d94c8618fcbf4c80c9762d51dfd68705461f
mc-server <marnix@hagehoeve.com>
2022-06-05 14:35:34 +0200

    fix: add email field

--- users/views.py
+++ users/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
-
+from users.forms import UserRegisterForm
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("index")
-        else:
-            return render(request, "users/register.html", {"form": form})
     else:
-        form = UserCreationForm()
-        return render(request, "users/register.html", {"form": form})
+        form = UserRegisterForm()
+    return render(request, "users/register.html", {"form": form})
 
 
 # def login(request):
