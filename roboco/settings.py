7f74296377c6cca56376d0407166b9741cff2451
mc-server <marnix@hagehoeve.com>
2022-06-05 23:40:45 +0200

    dev: add production db support

--- roboco/settings.py
+++ roboco/settings.py
@@ -88,12 +88,24 @@ WSGI_APPLICATION = "roboco.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.getenv("PYTHON_ENV") == "production":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "roboco",
+            "USER": "roboco",
+            "PASSWORD": "roboco",
+            "HOST": "postgres",
+            "PORT": "5432",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
