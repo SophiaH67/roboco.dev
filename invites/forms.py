cfe221908c3bb9ffb60f9679b9e8dd3ddc3b306e
mc-server <marnix@hagehoeve.com>
2022-06-05 23:31:29 +0200

    feat: email template

--- invites/forms.py
+++ invites/forms.py
@@ -3,6 +3,7 @@ from invites.decorators import create_invite_code
 from users.models import User
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 
 class InviteUserForm(forms.Form):
@@ -24,18 +25,19 @@ class InviteUserForm(forms.Form):
         message = self.cleaned_data["message"]
 
         context = {
-            "email": email,
             "message": message,
-            "invite_link": create_invite_code(self.inviter),
+            "invite_link": f"https://roboco.dev/register/?invite={create_invite_code(self.inviter)}",
             "inviter": self.inviter,
         }
 
-        message = render_to_string("invites/email_body.html", context)
+        html_message = render_to_string("invites/email_body.html", context)
+        plain_message = strip_tags(html_message)
 
         send_mail(
             "Invitation to join",
-            message,
+            plain_message,
             "robocosa@roboco.dev",
             [email],
             fail_silently=False,
+            html_message=html_message,
         )
