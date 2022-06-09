9e9460b4d1ed0aee06a4c33a922d3d5e6c58b38d
marnixah <marnix@hagehoeve.com>
2022-06-09 21:23:20 +0200

    dev: consistent return messages

--- nginx/views.py
+++ nginx/views.py
@@ -8,11 +8,11 @@ services = [service for code, name, service in nginx_permissions]
 def return_nginx_response(request, service):
     # If user is not logged in, return 401
     if not request.user.is_authenticated:
-        return HttpResponse(status=401)
+        return HttpResponse("Unauthorized", status=401)
     try:
         index = services.index(service)
     except ValueError:
-        return HttpResponse(status=404)
+        return HttpResponse("Service not found", status=404)
     permission = nginx_permissions[index][0]
     # Check if the user has the permission to access the service
     if request.user.has_perm(permission):
