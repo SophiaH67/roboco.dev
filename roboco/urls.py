c0fbdf0cd185fe12d42660d4eecbd8380423d066
marnixah <marnix@hagehoeve.com>
2022-06-09 23:03:31 +0200

    fix: redirect logged in users when /login

--- roboco/urls.py
+++ roboco/urls.py
@@ -28,7 +28,9 @@ urlpatterns = [
     path("register/", user_views.register, name="register"),
     path(
         "login/",
-        auth_views.LoginView.as_view(template_name="users/login.html"),
+        auth_views.LoginView.as_view(
+            template_name="users/login.html", redirect_authenticated_user=True
+        ),
         name="login",
     ),
     path(
