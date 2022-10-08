dc8cc070945d1178799de7f80ef848dab674ee53
marnixah <marnix@roboco.dev>
2022-10-08 13:44:04 +0200

    fix(sharex): fix upload page name

--- sharex/urls.py
+++ sharex/urls.py
@@ -3,7 +3,7 @@ from .views import upload, view_image, serve_image, create_token
 from django.conf import settings
 
 urlpatterns = [
-    path("s/upload", upload, name="index"),
+    path("s/upload", upload, name="upload"),
     path("s/<str:id>/", view_image, name="view"),
     path("sharex/<str:filename>/", serve_image, name="serve"),
     path("s/create_token", create_token, name="create_token"),
