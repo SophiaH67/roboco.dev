d25efec467657c11c85574cf0a697d229edac7e6
Sophia Hage <sophia@sophiah.gay>
2025-12-16 14:18:04 +0100

    fix: :truck: profile security route

--- users/urls.py
+++ users/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
         name="logout",
     ),
     path("profile/", user_views.profile, name="profile"),
-    path("profile/security/", user_views.profile_security, name="profile"),
+    path("profile/security/", user_views.profile_security, name="security"),
 ]
